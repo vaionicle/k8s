90d6871cdd51a6fead292a1c7bef581624a11736
Vasilis Tsaknis <tsaknis_vasilios@hotmail.com>
2023-01-18 15:49:12 +0200

    Improving ubuntu server installation process

--- src/scripts/install_ubuntu.sh
+++ src/scripts/install_ubuntu.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-VM_COUNT=4
+NODE=${1:-""}
+
+if [ "${NODE}" == "" ]; then
+    echo "NODE ID is not set"
+    echo "./src/scripts/install_ubuntu.sh 1..N"
+    return
+fi
+
+VM_GROUP="k8s"
+VM_LOCATION="/opt/VirtualBox"
 
 ISO_VERSION="22.04.1"
 ISO_FILE="ubuntu-${ISO_VERSION}-live-server-amd64.iso"
@@ -8,16 +17,20 @@ ISO_FILE="ubuntu-${ISO_VERSION}-live-server-amd64.iso"
 [ ! -f "${ISO_FILE}" ] && wget https://releases.ubuntu.com/${ISO_VERSION}/${ISO_FILE}
 
 tmpdir=./src/configs
+vm_name="k8s_node_${NODE}"
 
-for i in $(seq ${VM_COUNT}); do
-	vm_name="k8s_node_${i}"
+metadata=$"instance-id: kuberneters-node-${NODE}"$'\nlocal-hostname: k8snode'${NODE}
+echo "$metadata" > ./src/configs/meta-data
 
-    metadata=$"instance-id: id-${i}"$'\nlocal-hostname: k8s-node'${i}
+mkisofs -JR -V cidata -o ./seed_${NODE}.iso ${tmpdir}
 
-    echo "$metadata" > ./src/configs/meta-data
+DESC=$'username: user\npassword: ubuntu\nhostname: k8snode'"${NODE}"
+VBoxManage modifyvm ${vm_name} --description "$(echo "$DESC")"
 
-    mkisofs -JR -V cidata -o ./seed_${i}.iso ${tmpdir}
+VBoxManage storageattach ${vm_name} --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium ${ISO_FILE}
+VBoxManage storageattach ${vm_name} --storagectl "SATA" --port 2 --device 0 --type dvddrive --medium seed_${NODE}.iso
 
-    # VBoxManage storageattach ${vm_name} --storagectl "SATA" --port 1 --device 0 --type dvddrive --medium ${ISO_FILE}
-    # VBoxManage storageattach ${vm_name} --storagectl "SATA" --port 2 --device 0 --type dvddrive --medium ./seed_${i}.iso
-done
+VBoxManage startvm ${vm_name} --type=gui
+
+tree ${VM_LOCATION}/${VM_GROUP}
+du -h ${VM_LOCATION}/${VM_GROUP}
